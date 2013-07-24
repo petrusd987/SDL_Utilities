1f12994a0331975f1a67f13aaa1db2dfc51197d9
petrusd987 <petrusd987@Peters-Gaming-Ubuntu>
2013-07-24 14:48:17 -0500

    Test commit from Netbeans.

---
+++ SpriteSheetReader.cpp
@@ -0,0 +1,9 @@
+/* 
+ * File:   SpriteSheetReader.cpp
+ * Author: petrusd987
+ * 
+ * Created on July 24, 2013, 2:47 PM
+ */
+
+#include "SpriteSheetReader.h"
+
---
+++ SpriteSheetReader.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   SpriteSheetReader.h
+ * Author: petrusd987
+ *
+ * Created on July 24, 2013, 2:47 PM
+ */
+
+#ifndef SPRITESHEETREADER_H
+#define	SPRITESHEETREADER_H
+
+class SpriteSheetReader {
+        public:
+        private:
+
+};
+
+#endif	/* SPRITESHEETREADER_H */
+
